c68258ab892dc2825aefae3cea61bb28985a47b3
Balwant0340 <balwantchoudhary0340@gmail.com>
2026-08-13 18:15:33 +0530

    Time: 0 ms (100%), Space: 8.9 MB (98.51%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <climits>
+
+using namespace std;
+
+class Solution {
+public:
+    int myAtoi(string s) {
+        long long ans = 0;
+        int idx = -1;
+        bool isNegative = false;
+        
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] != ' ') {
+                idx = i;
+                break;
+            }
+        }
+        if (idx == -1) return 0;
+        if (s[idx] == '-' || s[idx] == '+') {
+            if (s[idx] == '-') {
+                isNegative = true;
+            }
+            idx++;
+        }
+        for (int i = idx; i < s.size(); ++i) {
+            if (s[i] < '0' || s[i] > '9') {
+                break;
+            }
+            int digit = s[i] - '0';
+            ans = ans * 10 + digit;
+            if (!isNegative && ans > INT_MAX) {
+                return INT_MAX;
+            }
+            if (isNegative && -ans < INT_MIN) {
+                return INT_MIN;
+            }
+        }
+        
+        if (isNegative) {
+            ans *= -1;
+        }
+        
+        return (int)ans;
+    }
+};
